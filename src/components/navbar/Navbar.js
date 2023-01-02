cefccd7cecac05d48d56a328224e5a7c78fcf038
Zain <47285447+ZainMalik00@users.noreply.github.com>
2023-01-02 16:41:05 -0500

    Added Firestore API

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,17 +6,18 @@ class Navbar extends React.Component {
 
     render(){
         return(
-            <nav className="navbar navbar-expand-lg navbar-light bg-success px-2">
+            <nav className="navbar navbar-expand-lg px-2" id="customNav">
                 <a className="navbar-brand" href="#">HOUR TRACKER</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMarkup" aria-controls="navbarMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMarkup" aria-controls="navbarMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-item nav-link active" href="#">Home</a>
-                        <a class="nav-item nav-link" href="#">Insert</a>
-                        <a class="nav-item nav-link" href="#">Charts</a>
-                        <a class="nav-item nav-link" href="#">Analytics</a>
+                <div className="collapse navbar-collapse" id="navbarMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link active" href="#">Home</a>
+                        <a className="nav-item nav-link" href="#">Insert</a>
+                        <a className="nav-item nav-link" href="#">Charts</a>
+                        <a className="nav-item nav-link" href="#">Analytics</a>
+                        <a className="nav-item nav-link" href="#">Sign In</a>
                     </div>
                 </div>
             </nav>
