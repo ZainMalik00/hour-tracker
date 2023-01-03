fdb3237f46138ac25579e8ab603f2bab2bd09679
Zain <47285447+ZainMalik00@users.noreply.github.com>
2023-01-03 03:44:29 -0500

    Added Early SignIn/SignOut Functionality

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { auth, provider } from '../../backend/firebase.js'
+import { signInWithPopup, signOut } from 'firebase/auth';
+
 import './Navbar.css';
 import '../../index.css';
 
-class Navbar extends React.Component {
+function Navbar() {
+    const [isAuth, setIsAuth] = useState(false);
+    const [username, setUsername] = useState("Login");
+    
+    useEffect(() => {
+        const name = localStorage.getItem('name');
+        const authExists = localStorage.getItem('isAuth');
+        
+        if(name) {
+            setUsername(name);
+        }
 
-    render(){
-        return(
+        if(authExists){
+            setIsAuth(authExists);
+        }
+
+      }, []);
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider).then((result) => {
+            localStorage.setItem("isAuth", true);
+            localStorage.setItem("name", result.user.displayName);
+            setIsAuth(true);
+            setUsername(result.user.displayName);
+            
+        },
+        (error) => {
+            setUsername("Login");
+        }); 
+    };
+
+    const signOutUser = () => {
+        if (window.confirm("Are You Sure You Want To Sign Out?")) {
+            signOut(auth).then(() => {
+                localStorage.clear();
+                setIsAuth(false);
+                setUsername("Login");
+            }, (error) => {});
+        }
+    };
+
+    return(
+        <Router>
             <nav className="navbar navbar-expand-lg px-2" id="customNav">
                 <a className="navbar-brand" href="#">HOUR TRACKER</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMarkup" aria-controls="navbarMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,12 +60,15 @@ class Navbar extends React.Component {
                         <a className="nav-item nav-link" href="#">Insert</a>
                         <a className="nav-item nav-link" href="#">Charts</a>
                         <a className="nav-item nav-link" href="#">Analytics</a>
-                        <a className="nav-item nav-link" href="#">Sign In</a>
+                        <a className="nav-item nav-link" href="#" onClick={isAuth ? signOutUser : signInWithGoogle}>{username}</a>
                     </div>
                 </div>
             </nav>
-        );
-    }
-}
+            <Routes>
+                {/* <Route path="/login" element={<span onClick={signInWithGoogle} />} /> */}
+            </Routes>
+        </Router>
+    );
+};
 
 export default Navbar;
